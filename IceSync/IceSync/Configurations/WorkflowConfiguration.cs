9b21d89c85645851c4a984eddc0bcc1e5daf659c
gentrit-maloku <gentritmaloku2@gmail.com>
2025-04-17 12:23:21 +0200

    feat: create table workflow

---
+++ IceSync/IceSync/Configurations/WorkflowConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using IceSync.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IceSync.Configurations
+{
+    public sealed record WorkflowConfiguration : IEntityTypeConfiguration<Workflow>
+    {
+        public void Configure(EntityTypeBuilder<Workflow> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(x => x.Name)
+                .IsRequired();
+
+            builder.Property(x => x.IsActive);
+
+            builder.Property(x => x.MultiExecBehavior);
+        }
+    }
+}
